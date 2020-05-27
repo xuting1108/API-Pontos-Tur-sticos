6b7231655d2df7a89817a43f2ecb3e1d25820088
Filipe <xuting1108@hotmail.com>
2020-05-27 17:27:46 -0300

    Add modifications

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from atracoes.models import Atracao
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    desricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+    
+    def __str__(self):
+        return self.nome
