1114c455105676730fb2a98b23719c369e5c79cc
Filipe <xuting1108@hotmail.com>
2020-05-29 19:00:21 -0300

    Add avaliacoes in models and settings and add migrations

--- comentarios/models.py
+++ comentarios/models.py
@@ -9,5 +9,5 @@ class Comentario(models.Model):
     aprovado = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.usuario.first_name
+        return self.usuario.username
     
