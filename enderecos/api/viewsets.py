62e82b7015506077179b216731c10a27e3819df1
Filipe <xuting1108@hotmail.com>
2020-06-03 18:37:21 -0300

    Add enderecos  endpoint

---
+++ enderecos/api/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from enderecos.models import Endereco
+from .serializers import EnderecoSerializer
+
+class EnderecosViewSet(ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
