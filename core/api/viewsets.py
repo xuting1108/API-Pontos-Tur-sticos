7a01434e806a7b3b1409f7c490071ba682525ad3
Filipe <xuting1108@hotmail.com>
2020-06-15 18:38:00 -0300

    Overide get_queryset method(list and create)

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,9 +2,17 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
+from rest_framework.response import Response
+
+
 class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
     
+    def list(self, request, *args, **kwargs):
+        return Response({'teste':123})
+    
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})
